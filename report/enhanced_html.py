f97a0bd044d1a3295995bae4d66293a00284c7df
Grant Gould <ggould@tri.global>
2017-07-25 11:24:25 -0400

    Remove unhelpful CSS block because I do not actually know CSS.

--- report/enhanced_html.py
+++ report/enhanced_html.py
@@ -27,12 +27,6 @@ HEADER = """
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<style>
-.floating-box {
-    align: right;
-    float: right;
-}
-</style>
 <html>
 """
 

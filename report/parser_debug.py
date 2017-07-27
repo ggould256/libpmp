3753c65b83737a9de4d13f9899e6ecc97adbe855
Grant Gould <ggould@tri.global>
2017-07-27 08:51:49 -0400

    Lint on parser_debug.

--- report/parser_debug.py
+++ report/parser_debug.py
@@ -15,6 +15,6 @@
 """Simple report that prints the structure of the parse tree of the model."""
 
 
-def report(root, args):
+def report(root, _):
     """Write out the whole node @p root in parser pretty-print form."""
     root.pretty_print()

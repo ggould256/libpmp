07ccd03073f133533661ec01970b2f8a4e807343
Grant Gould <ggould@alum.mit.edu>
2026-03-12 19:42:45 -0400

    Test workflow

---
+++ test/run_tests.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -exuo pipefail
+
+# Commands from README.md
+python3 -m venv .test_venv
+.test_venv/bin/python3 -m pip install --upgrade pip
+.test_venv/bin/pip install -e .
+.test_venv/bin/ruff check .
+.test_venv/bin/py.test
